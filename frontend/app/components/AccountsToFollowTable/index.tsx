54a818f1c72f5b5691a7620e1e227924ea21b941
Richard Zilahi <zilahi@gmail.com>
2025-09-26 23:44:03 +0300

    implementing data table

--- frontend/app/components/AccountsToFollowTable/index.tsx
+++ frontend/app/components/AccountsToFollowTable/index.tsx
@@ -12,7 +12,14 @@ import {
   flexRender,
   getCoreRowModel,
   useReactTable,
+  getPaginationRowModel,
+  type SortingState,
+  getSortedRowModel,
 } from "@tanstack/react-table";
+import { Button } from "../ui/button";
+import { Badge } from "../ui/badge";
+import { useState } from "react";
+import { ArrowUpDownIcon } from "lucide-react";
 
 type AccountToFollow = CampaignFollowers;
 
@@ -27,22 +34,61 @@ const columns: ColumnDef<AccountToFollow>[] = [
   },
   {
     accessorKey: "me_following",
-    header: "Am I Currently Following?",
-    cell: ({ row }) => (row.getValue("me_following") ? "Yes" : "No"),
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Me following
+          <ArrowUpDownIcon className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) =>
+      row.getValue("me_following") ? (
+        <Badge variant="success">Yes</Badge>
+      ) : (
+        <Badge variant="destructive">No</Badge>
+      ),
   },
   {
     accessorKey: "is_following_me",
-    header: "Is Following Me",
-    cell: ({ row }) => (row.getValue("is_following_me") ? "Yes" : "No"),
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Is following me
+          <ArrowUpDownIcon className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) =>
+      row.getValue("is_following_me") ? (
+        <Badge variant="success">Yes</Badge>
+      ) : (
+        <Badge variant="destructive">No</Badge>
+      ),
   },
 ];
 
 function AccountsToFollowTable({ data }: IAccountsToFollowTable) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
   });
+
   return (
     <div>
       <div className="overflow-hidden rounded-md border">
@@ -94,6 +140,32 @@ function AccountsToFollowTable({ data }: IAccountsToFollowTable) {
             )}
           </TableBody>
         </Table>
+      </div>
+      <div className="flex flex-row justify-between items-center">
+        <div>
+          <p className="text-sm text-muted-foreground">
+            Page {table.getState().pagination.pageIndex + 1} of{" "}
+            {table.getPageCount()}
+          </p>
+        </div>
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
       </div>
     </div>
   );
